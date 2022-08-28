6f7bf27cdffd38a8907fd3caace91efc2e3e2e2a
daomingQian <813928419@qq.com>
2022-08-28 22:41:44 +0800

    java入门第一天

---
+++ java01/Holle.java
@@ -0,0 +1,6 @@
+public class Holle {
+    public static void main(String[] args) {
+        System.out.println("holle world");
+        System.out.println("chenqian is studing java");
+    }
+}
---
+++ java01/笔记.t
@@ -0,0 +1,3 @@
+1. jdk(java开发包) > jre(java运行环境) > jvm(java虚拟机)
+2. 配置环境变量 (1) JAVA_HOME: 'jdk文件地址' (2) 上面的path: 加一个 %JAVA_HOME%\bin;
+3. java代码 编译--> class -->运行 window|Linux|mac;
