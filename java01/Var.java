1c81af85d21f9bab373e80966d35f7578d6dd368
daomingQian <813928419@qq.com>
2022-08-31 22:41:02 +0800

    java第四天

---
+++ java01/Var.java
@@ -0,0 +1,21 @@
+public class Var {
+    public static void main(String[] args) {
+        byte a = 1;
+        System.out.println(a);
+        short b = 2;
+        System.out.println(b);
+        int c = 3;
+        System.out.println(c);
+        long d = 4L;
+        System.out.println(d);
+
+        boolean flag = true;
+        System.out.println(flag);
+        char ch = 97;
+        System.out.println(ch);
+        float num1 = 98.2f;
+        System.out.println(num1);
+        double num2 = 100.23;
+        System.out.println(num2);
+    }
+}
