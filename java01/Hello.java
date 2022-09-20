fe9901e78df34a0d6078916f01ccb3b675367958
daomingQian <813928419@qq.com>
2022-09-20 22:01:25 +0800

    java第9天

--- java01/Hello.java
+++ java01/Hello.java
@@ -20,3 +20,13 @@ class Loop {
     }
 
 }
+
+class Array {
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+            System.out.println(arr[i]);
+        }
+    }
+}
